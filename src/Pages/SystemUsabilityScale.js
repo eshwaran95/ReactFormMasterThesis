5b40255f91ff3da208f5f988bae7138e296408d4
eshwaran95 <31202474+eshwaran95@users.noreply.github.com>
2020-07-20 16:24:00 +0200

    Usability Test Introduction is done

--- src/Pages/SystemUsabilityScale.js
+++ src/Pages/SystemUsabilityScale.js
@@ -1,34 +1,141 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@material-ui/icons/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAltOutlined';
+import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
-export default function SystemUsabilityScale() {
-  const [value, setValue] = React.useState(2);
+const StyledRating = withStyles({
+  iconFilled: {
+    color: '#ff6d75',
+  },
+  iconHover: {
+    color: '#ff3d47',
+  },
+})(Rating);
 
+const customIcons = {
+  1: {
+    icon: <SentimentVeryDissatisfiedIcon />,
+    label: 'Very Dissatisfied',
+  },
+  2: {
+    icon: <SentimentDissatisfiedIcon />,
+    label: 'Dissatisfied',
+  },
+  3: {
+    icon: <SentimentSatisfiedIcon />,
+    label: 'Neutral',
+  },
+  4: {
+    icon: <SentimentSatisfiedAltIcon />,
+    label: 'Satisfied',
+  },
+  5: {
+    icon: <SentimentVerySatisfiedIcon />,
+    label: 'Very Satisfied',
+  },
+
+};
+
+function IconContainer(props) {
+  const { value, ...other } = props;
+  return <span {...other}>{customIcons[value].icon}</span>;
+}
+
+IconContainer.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+export default function SystemUsabilityScale() {
   return (
     <div>
-      <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Controlled</Typography>
-        <Rating
-          name="simple-controlled"
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
+            <Box component="fieldset" mb={3} borderColor="transparent">
+        <Typography component="legend">I think that I would like to use this device frequently</Typography>
+        <StyledRating
+          name="customized-color"
+          defaultValue={2}
+          getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+          precision={0.5}
+          icon={<FavoriteIcon fontSize="inherit" />}
         />
       </Box>
       <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Read only</Typography>
-        <Rating name="read-only" value={value} readOnly />
+        <Typography component="legend">I found the device unnecessarily complex</Typography>
+        <Rating
+          name="customized-icons"
+          defaultValue={2}
+          getLabelText={(value) => customIcons[value].label}
+          IconContainerComponent={IconContainer}
+        />
       </Box>
       <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Disabled</Typography>
-        <Rating name="disabled" value={value} disabled />
+        <Typography component="legend">I thought the device was easy to use</Typography>
+        <StyledRating
+          name="customized-color"
+          defaultValue={2}
+          getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+          precision={0.5}
+          icon={<FavoriteIcon fontSize="inherit" />}
+        />
       </Box>
       <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Pristine</Typography>
-        <Rating name="pristine" value={null} />
+        <Typography component="legend">I think that I would need the support of a technical person to be able to use this device</Typography>
+        <StyledRating
+          name="customized-color"
+          defaultValue={2}
+          getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+          precision={0.5}
+          icon={<FavoriteIcon fontSize="inherit" />}
+        />
+      </Box>
+      <Box component="fieldset" mb={3} borderColor="transparent">
+        <Typography component="legend">I found the device very cumbersome to use</Typography>
+        <StyledRating
+          name="customized-color"
+          defaultValue={2}
+          getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+          precision={0.5}
+          icon={<FavoriteIcon fontSize="inherit" />}
+        />
+      </Box>
+      <Box component="fieldset" mb={3} borderColor="transparent">
+        <Typography component="legend">I thought there was too much inconsistency in this device</Typography>
+        <Rating
+          name="customized-empty"
+          defaultValue={2}
+          precision={0.5}
+          emptyIcon={<StarBorderIcon fontSize="inherit" />}
+        />
+      </Box>
+      <Box component="fieldset" mb={3} borderColor="transparent">
+        <Typography component="legend">I would imagine that most people would learn to use this device very quickly</Typography>
+        <Rating
+          name="customized-empty"
+          defaultValue={2}
+          precision={0.5}
+          emptyIcon={<StarBorderIcon fontSize="inherit" />}
+        />
+      </Box>
+      <Box component="fieldset" mb={3} borderColor="transparent">
+        <Typography component="legend">I felt very confident using the device</Typography>
+        <Rating name="customized-10" defaultValue={2} max={10} />
+      </Box>
+      <Box component="fieldset" mb={3} borderColor="transparent">
+        <Typography component="legend">I needed to learn a lot of things before I could get going with this device</Typography>
+        <Rating
+          name="customized-icons"
+          defaultValue={2}
+          getLabelText={(value) => customIcons[value].label}
+          IconContainerComponent={IconContainer}
+        />
       </Box>
     </div>
   );
