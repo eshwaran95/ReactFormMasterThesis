d91d881480f8ded91993d32c95a7b1a8623c9b41
eshwaran95 <31202474+eshwaran95@users.noreply.github.com>
2020-07-21 01:23:25 +0200

    i REMOVED

--- src/Pages/UiSpecifications.js
+++ src/Pages/UiSpecifications.js
@@ -1,4 +1,4 @@
-iimport React from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
